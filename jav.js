3890799229212df979d0c8df021c7c98243f46d6
Nguyen Xuan Toan <96753996+Neeeap@users.noreply.github.com>
2025-07-27 15:52:35 +0700

    changed font and rephrased landing page link

--- jav.js
+++ jav.js
@@ -1,9 +1,9 @@
 const links=document.querySelectorAll("h2");
 links.forEach((link) =>{
     link.addEventListener("mouseenter", () =>{
-        link.style.cssText= "padding: 1%; background-color: cyan; border-radius: 10px"
+        link.style.cssText= "background-color: cyan; border-radius: 10px"
     })
     link.addEventListener("mouseleave", () =>{
-        link.style.cssText= "padding: 1%; background-color: white; border-radius: 10px"
+        link.style.cssText= "background-color: white; border-radius: 10px"
     })
 })
